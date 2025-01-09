e1f049e78ab36d73d9096007a986c11cb87f05c6
Yashw Raghavendra <yashw@example.com>
2025-01-09 20:29:41 +0530

    files added

---
+++ s.raghavendra/src/Student1.java
@@ -0,0 +1,22 @@
+public class Student1{
+    String name;
+ String dept;
+String classno;
+public void talking() {
+System.out.println("students are takilng");
+
+}
+public void sleeping(){
+    System.out.println(" students are sleeping");
+}
+
+    
+}
+ class Test{
+
+
+
+public static void main (String[] args){
+    Student1 std1 = new Student1();
+}
+}
---
+++ s.raghavendra/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Student1 std1 = new Student1();
+        System.out.println(std1.name);
+        std1.name = "Raghavendra";
+        std1.classno = "g2";
+        std1.dept = "cse";
+        
+    
+    System.out.println(std1);
+}
+
+}
